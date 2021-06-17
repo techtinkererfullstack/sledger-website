b60346f36e12003229db00f64f3a20bebf26b939
shafe <alam.shafee@gmail.com>
2021-06-17 16:47:47 +0600

    home page comepleted for mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+// selecting elements on section 5
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
+// selecting elements on section 6
+const pricing_tabs = document.querySelectorAll(".pricing__tab");
+const pricing_tabsContainer = document.querySelector(".pricing__tab-container");
+const pricing_tabsContent = document.querySelectorAll(".pricing__content");
+// selecting faq elements
+const faq_container = document.querySelector(".faq-container");
+const questions_container = document.querySelectorAll(".question-container");
 
-// implementing tabes in section
+console.log(faq_container);
 
+// implementing tabes in section 5
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
 
@@ -19,4 +28,29 @@ tabsContainer.addEventListener("click", function (e) {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
+});
+
+// implementing tabes in section 6
+pricing_tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".pricing__tab");
+
+  if (!clicked) return;
+
+  pricing_tabs.forEach((tab) => tab.classList.remove("pricing__tab--active"));
+  pricing_tabsContent.forEach((content) =>
+    content.classList.remove("pricing__content--active")
+  );
+
+  clicked.classList.add("pricing__content--active");
+  clicked.classList.add("pricing__tab--active");
+  document
+    .querySelector(`.pricing__content--${clicked.dataset.tab}`)
+    .classList.add("pricing__content--active");
+});
+
+// implementing faq revealing answers
+faq_container.addEventListener("click", function (e) {
+  const answer = e.target.nextElementSibling;
+  // if (answer.classlist.contain("answer"))
+  answer.classList.toggle("hidden");
 });
